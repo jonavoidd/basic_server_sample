5adc0c2c3ce080b8b3620421079e24db81e9432b
Kurt <kurtjonathangozano@gmail.com>
2024-09-03 09:53:22 +0800

    edited routes

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const profile = require("./profile");
 
 router.get("/", (req, res) => {
   res.send(
-    `Welcome to this server. To get started, try adding ["/firstname", "/lastname", "/age", "/school-email", "/my-data"] in the url`
+    `Welcome to this server. To get started, try adding ["/firstname", "/lastname", "/age", "/schoolemail", "/mydata"] in the url`
   );
 });
 
@@ -20,11 +20,11 @@ router.get("/age", (req, res) => {
   res.send(profile.age);
 });
 
-router.get("/school-email", (req, res) => {
+router.get("/schoolemail", (req, res) => {
   res.send(profile.schoolEmail);
 });
 
-router.get("/my-data", (req, res) => {
+router.get("/mydata", (req, res) => {
   res.send(profile);
 });
 
